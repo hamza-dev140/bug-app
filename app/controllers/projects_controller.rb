4d6de37c9807957e860025ee31a2ae9bddbb1d5c
hamza-dev140 <162440951+hamza-dev140@users.noreply.github.com>
2024-03-15 02:59:06 +0500

    first

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,14 @@
+class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_and_belongs_to_many :projects
+  enum role: {manager: 0, quality_assurance: 1, developer: 2}
+end
